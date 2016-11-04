509621a20171f89b80d0fd9d306a6663899be071
Harminder Virk <virk.officials@gmail.com>
2016-11-04 16:24:30 +0530

    style(signature): add file signature

--- middleware/Ally.js
+++ middleware/Ally.js
@@ -1,5 +1,14 @@
 'use strict'
 
+/*
+ * adonis-ally
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
 class Ally {
 
   * handle (request, response, next) {
