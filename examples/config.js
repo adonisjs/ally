d265ac4b638eb66bb6311b286d518537ee97a77a
Harminder Virk <virk.officials@gmail.com>
2016-11-05 16:41:14 +0530

    style(lint): make linter happy

--- examples/config.js
+++ examples/config.js
@@ -47,7 +47,6 @@ module.exports = {
       redirectUri: `${Env.get('APP_URL')}/authenticated/google`
     },
 
-
     /*
     |--------------------------------------------------------------------------
     | Github Configuration
