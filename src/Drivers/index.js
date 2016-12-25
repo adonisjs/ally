c14f61bf8a1ed911fdfd8540261ce8e08daed1f7
Ayeni Olusegun <nsegun5@gmail.com>
2016-12-25 17:50:38 +0100

    feat(social-auth):create social login via instagram

--- src/Drivers/index.js
+++ src/Drivers/index.js
@@ -14,5 +14,6 @@ module.exports = {
   github: require('./Github'),
   google: require('./Google'),
   linkedin: require('./LinkedIn'),
-  twitter: require('./Twitter')
+  twitter: require('./Twitter'),
+  instagram: require('./Instagram')
 }
