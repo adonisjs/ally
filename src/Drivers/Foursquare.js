8fb43bb75674920a67336afdc4d8bf6ef97d4b8b
Ayeni Olusegun <nsegun5@gmail.com>
2017-01-27 17:05:54 +0100

    feat(social-auth): create foursquare driver

---
+++ src/Drivers/Foursquare.js
@@ -0,0 +1,171 @@
+/**
+ * Created by Raphson on 1/1/17.
+ */
+'use strict'
+
+/*
+ * adonis-ally
+ *
+ * (c) Ayeni Olusegun <nsegun5@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const CE = require('../Exceptions')
+const OAuth2Scheme = require('../Schemes/OAuth2')
+const AllyUser = require('../AllyUser')
+const got = require('got')
+const utils = require('../../lib/utils')
+const _ = utils.mixLodash(require('lodash'))
+
+class Foursquare extends OAuth2Scheme {
+
+  constructor (Config) {
+    const config = Config.get('services.ally.foursquare')
+
+    if (!_.hasAll(config, ['clientId', 'clientSecret', 'redirectUri'])) {
+      throw CE.OAuthException.missingConfig('foursquare')
+    }
+
+    super(config.clientId, config.clientSecret, config.headers)
+
+        /**
+         * Oauth specific values to be used when creating the redirect
+         * url or fetching user profile.
+         */
+    this._redirectUri = config.redirectUri
+    this._redirectUriOptions = _.merge({response_type: 'code'}, config.options)
+  }
+
+    /**
+     * Injections to be made by the IoC container
+     *
+     * @return {Array}
+     */
+  static get inject () {
+    return ['Adonis/Src/Config']
+  }
+
+    /**
+     * Base url to be used for constructing
+     * facebook oauth urls.
+     *
+     * @return {String}
+     */
+  get baseUrl () {
+    return 'https://foursquare.com/'
+  }
+
+    /**
+     * Relative url to be used for redirecting
+     * user.
+     *
+     * @return {String} [description]
+     */
+  get authorizeUrl () {
+    return 'oauth2/authenticate'
+  }
+
+    /**
+     * Relative url to be used for exchanging
+     * access token.
+     *
+     * @return {String}
+     */
+  get accessTokenUrl () {
+    return 'oauth2/access_token'
+  }
+
+    /**
+     * Returns the user profile as an object using the
+     * access token
+     *
+     * @param   {String} accessToken
+     *
+     * @return  {Object}
+     *
+     * @private
+     */
+  * _getUserProfile (accessToken) {
+    const date = new Date()
+    const formattedDate = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}`
+    const profileUrl = `https://api.foursquare.com/v2/users/self?oauth_token=${accessToken}&m=foursquare&v=${formattedDate}`
+    const response = yield got(profileUrl, {
+      headers: {
+        'Accept': 'application/json'
+      },
+      json: true
+    })
+    return response.body
+  }
+
+    /**
+     * Returns the redirect url for a given provider.
+     *
+     *
+     * @return {String}
+     */
+  * getRedirectUrl () {
+    return this.getUrl(this._redirectUri, null, this._redirectUriOptions)
+  }
+
+    /**
+     * Parses the redirect errors returned by facebook
+     * and returns the error message.
+     *
+     * @param  {Object} queryParams
+     *
+     * @return {String}
+     */
+  parseRedirectError (queryParams) {
+    return queryParams.error || 'Oauth failed during redirect'
+  }
+
+    /**
+     * Returns the user profile with it's access token, refresh token
+     * and token expiry
+     *
+     * @param {Object} queryParams
+     *
+     * @return {Object}
+     */
+  * getUser (queryParams) {
+    const code = queryParams.code
+
+        /**
+         * Throw an exception when query string does not have
+         * code.
+         */
+    if (!code) {
+      const errorMessage = this.parseRedirectError(queryParams)
+      throw CE.OAuthException.tokenExchangeException(errorMessage, null, errorMessage)
+    }
+
+    const accessTokenResponse = yield this.getAccessToken(code, this._redirectUri, {
+      grant_type: 'authorization_code'
+    })
+    const userProfile = yield this._getUserProfile(accessTokenResponse.accessToken)
+    const avatarUrl = `${userProfile.response.user.photo.prefix}original${userProfile.response.user.photo.suffix}`
+    const user = new AllyUser()
+    user
+            .setOriginal(userProfile)
+            .setFields(
+                userProfile.response.user.id,
+                `${userProfile.response.user.firstName} ${userProfile.response.user.lastName}`,
+                userProfile.response.user.contact.email || null,
+                '',
+                avatarUrl
+            )
+            .setToken(
+                accessTokenResponse.accessToken,
+                accessTokenResponse.refreshToken,
+                null,
+                Number(_.get(accessTokenResponse, 'result.expires'))
+            )
+
+    return user
+  }
+}
+
+module.exports = Foursquare
