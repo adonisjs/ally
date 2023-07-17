f092b8f2f1b7ff34108717a7ebdafc40bcfe264f
Harminder Virk <virk.officials@gmail.com>
2023-07-17 12:18:08 +0530

    style: update license doc block

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 /*
  * @adonisjs/ally
  *
- * (c) Harminder Virk <virk@adonisjs.com>
+ * (c) AdonisJS
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,9 +11,10 @@ import './src/bindings/types.js'
 
 export { HttpClient as ApiRequest } from '@poppinss/oauth-client'
 
-export { Oauth2Driver } from './src/abstract_drivers/oauth2.js'
-export { Oauth1Driver } from './src/abstract_drivers/oauth1.js'
-export { AllyManager } from './src/ally_manager.js'
-export { default as driversList } from './src/drivers_collection.js'
 export * as errors from './src/exceptions.js'
+export { AllyManager } from './src/ally_manager.js'
+export { defineConfig } from './src/define_config.js'
 export { RedirectRequest } from './src/redirect_request.js'
+export { Oauth1Driver } from './src/abstract_drivers/oauth1.js'
+export { Oauth2Driver } from './src/abstract_drivers/oauth2.js'
+export { default as driversList } from './src/drivers_collection.js'
